1c82e324f42f27a84b61a8b67dabc176e97c8b1a
matfariasc <mat.fariasc@icloud.com>
2021-05-20 10:55:37 -0400

    Create practicadecondicionales.js

---
+++ practicadecondicionales.js
@@ -0,0 +1,30 @@
+//1
+var num = 15;
+if (num >= 10) {
+    console.log("el número es mayor que 10");
+}
+
+// Se muestra en la consola : "el número es mayor que 10"
+
+//2
+var name = "Bruce";
+if (name != "Bruce") {
+    console.log("¿Cuál es tu nombre?");
+} 
+else {
+    console.log("Hola, Bruce!");
+}
+
+// Se muestra en la consola : "Hola, Bruce!"
+
+//3
+var num1 = 10;
+var num2 = 15;
+if (num1 <= num2) {
+    num1 = num1 + 10;
+}
+console.log(num1);
+console.log(num2);
+
+// Se muestra en la consola : 20
+//                            15
