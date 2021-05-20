c5992c827a5e1e33b6855d76da225452a8b6e523
matfariasc <mat.fariasc@icloud.com>
2021-05-20 11:48:12 -0400

    Create practicabuclesfor.js

---
+++ practicabuclesfor.js
@@ -0,0 +1,29 @@
+//20-05-2021
+//1
+for (var a = 0; a < 8; a++) {   
+    console.log(a);
+}
+
+// Se muestra en la consola : 0,1,2,3,4,5,6,7
+
+//2
+for (var b = 10; b < 100; b += 10) {   
+    console.log(b);
+}
+
+// Se muestra en la consola : 10,20,30,40,50,60,70,80,90
+
+//3
+for (var c = 16; c > 0; c--) {
+        console.log(c);
+    }
+
+// Se muestra en la consola : 16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1
+
+//4
+for (var d = 0; d < 100; d++) {
+        console.log(d);
+        d += 5;
+    }
+
+// Se muestra en la consola : 0,6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96
